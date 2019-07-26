787f756e36c5237f16f56464c39e25f8480003bd
Solya589 <53277983+Solya589@users.noreply.github.com>
2019-07-26 19:38:08 +0300

    Update HW1.py

--- HW1/HW1.py
+++ HW1/HW1.py
@@ -3,18 +3,22 @@ import collections
 
 book = open("book.txt", "r")
 text= book.read()
-book.close()
 
-# I know this is cheating but apostrophe is sucks
-text_a = text.lower().replace('\'','APOS')
+wordsWithApos = re.findall(r"\w+\'\w+", text.lower())
 
-words = re.findall(r'\w+', text_a)
+wordsAll=text.lower().split()
+
+wordsWithoutApos = ' '.join([w for w in wordsAll if w not in wordsWithApos])
+
+words = re.findall(r"\w+", wordsWithoutApos)
+words += wordsWithApos
 
 counter=collections.Counter(words)
 
-for (key, value) in counter.items():
- 
- # I remember that was a condition print only words that repeated in text at least once. 
+
+for (key, value) in counter.items() :
+
+ # I remember that was a condition print only words that repeated in text at least once.
  # If I am wrong then if clause is unnecessary
   if value > 1:
-   print(key.replace('APOS','\''),  value, " times")
+   print(key,  value, " times ")
